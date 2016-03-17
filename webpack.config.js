f0fc9cd3e80e80219ced316769507876a9fff794
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-17 00:43:08 +0000

    各フォルダと各種設定ファイルのみ追加

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './js/index.js',
+  output: { path: 'dest', filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
